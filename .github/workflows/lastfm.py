c939b4c5280ac44e248fa56db2d5ffec5d7e7008
retra <jozalamun@gmail.com>
2026-02-04 22:04:12 -0500

    handle errors gracefully when using lastfm response

--- .github/workflows/lastfm.py
+++ .github/workflows/lastfm.py
@@ -1,3 +1,4 @@
+
 import requests
 import os
 import sys
@@ -11,7 +12,6 @@ def lastfm_request(payload):
                             headers=headers, params=payload)
     return response
 
-
 def get_weekly_album_chart():
     payload = {'method': 'user.getweeklyalbumchart'}
     data = lastfm_request(payload).json()['weeklyalbumchart']['album']
@@ -21,47 +21,64 @@ def get_weekly_album_chart():
                                 data[i]['name']])
     return artist_and_album
 
-
 def get_album_covers(artist_and_album):
     images = []
     for album in artist_and_album:
         payload = {'method': 'album.getinfo',
                    'artist': album[0],
                    'album': album[1]}
-        request_response = lastfm_request(payload).json()
-        url = request_response['album']['image'][2]['#text']
-        link_to_album = request_response['album']['url']
-        if (url != ''):
-            images.append([album[0], album[1], url, link_to_album])
+        try:
+            request_response = lastfm_request(payload).json()
+            
+            if 'error' in request_response:
+                print(f"Warning: API error for {album[0]} - {album[1]}: {request_response.get('message', 'Unknown error')}")
+                continue
+            
+            if 'album' not in request_response:
+                print(f"Warning: No album data found for {album[0]} - {album[1]}")
+                continue
+            
+            if 'image' not in request_response['album'] or len(request_response['album']['image']) < 3:
+                print(f"Warning: No image data for {album[0]} - {album[1]}")
+                continue
+                
+            url = request_response['album']['image'][2]['#text']
+            link_to_album = request_response['album']['url']
+            
+            if url != '':
+                images.append([album[0], album[1], url, link_to_album])
+        except Exception as e:
+            print(f"Warning: Error processing {album[0]} - {album[1]}: {str(e)}")
+            continue
+    
     return images
-
 
 def update_readme(images):
     with open('README.md', 'r', encoding='utf-8') as file:
         readme = file.readlines()
+    
     lastfm_line_index = readme.index('<!-- lastfm -->\n') + 1
     lastfm_line = '<p align="center">'
     i = 0
+    
     for img in images:
-        if (i < int(os.getenv('IMAGE_COUNT'))):
-            if (requests.get(img[2]).status_code == 200):
+        if i < int(os.getenv('IMAGE_COUNT')):
+            if requests.get(img[2]).status_code == 200:
                 if os.getenv('INCLUDE_LINK') == 'false':
                     lastfm_line += f'<img src="{img[2]}" title="{img[0]} - {img[1]}"> '
                 else:
                     lastfm_line += f'<a href="{img[3]}"><img src="{img[2]}" title="{img[0]} - {img[1]}"></a> '
-                # lastfm_line = lastfm_line + '![' + img[0] + ' - ' + img[1] + '](' + img[2] + ') '
                 i = i + 1
-            else:
-                pass
         else:
             break
-    if (readme[lastfm_line_index] == lastfm_line):
+    
+    if readme[lastfm_line_index] == lastfm_line:
         sys.exit(0)
     else:
         lastfm_line = lastfm_line + '</p>\n'
         readme[lastfm_line_index] = lastfm_line
+    
     with open('README.md', 'w', encoding='utf-8') as file:
         file.writelines(readme)
-
 
 update_readme(get_album_covers(get_weekly_album_chart()))
